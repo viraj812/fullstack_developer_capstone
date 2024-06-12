b44371d4f2bb874f4004e0ca4467c6cdcf5b8fed
Viraj <124186730+viraj812@users.noreply.github.com>
2024-06-12 13:20:22 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
+
 # from django.utils.timezone import now
+
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -14,6 +18,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - __str__ method to print a car make object
 
 class CarMake(models.Model):
+
     name = models.CharField(max_length=100)
     description = models.TextField()
 
@@ -32,22 +37,18 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 
 class CarModel(models.Model):
+
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
-    CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('HATCHBACK', 'Hatchback')
-    ]
+    CAR_TYPES = [('SEDAN', 'Sedan'), ('SUV', 'SUV'), ('WAGON', 'Wagon'
+                 ), ('HATCHBACK', 'Hatchback')]
 
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2024, 
-        validators=[
-            MaxValueValidator(2024),
-            MinValueValidator(2015)
-        ])
-    
+    type = models.CharField(max_length=10, choices=CAR_TYPES,
+                            default='SUV')
+    year = models.IntegerField(default=2024,
+                               validators=[MaxValueValidator(2024),
+                               MinValueValidator(2015)])
+
     def __str__(self):
         return self.name
